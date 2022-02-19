fe92fc5541afe13b334f6d640a3b700861996fc1
Evgeny <e.archibasov@gmail.com>
2022-02-20 04:31:57 +1000

    [server] Server init

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
     './src/**/*.{ts,tsx}': () => 'yarn type-check',
     './src/**/*.{js,jsx,ts,tsx}': ['yarn prettify', 'yarn lint', 'yarn stylelint'],
+    './server/**/*.{js,ts}': ['yarn type-check', 'yarn prettify', 'yarn lint']
 }
