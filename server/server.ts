05055de79e9ef95273684b5380c334c3fb3b06ab
Evgeny <e.archibasov@gmail.com>
2022-02-23 02:02:34 +1000

    [server] test

--- server/server.ts
+++ server/server.ts
@@ -3,18 +3,23 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    const { name } = require('url').parse(req.url, true).query;
+const server = http.createServer(
+    (
+        req: { url: string },
+        res: { statusCode: number; setHeader: (arg0: string, arg1: string) => void; end: (arg0: string) => void }
+    ) => {
+        const { name } = require('url').parse(req.url, true).query;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
 
-    if (name !== undefined) {
-        res.end(`Hello ${name}`);
-    } else {
-        res.end(`Hello world`);
+        if (name !== undefined) {
+            res.end(`Hello ${name}`);
+        } else {
+            res.end(`Hello world`);
+        }
     }
-});
+);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
