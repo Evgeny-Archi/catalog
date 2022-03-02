a4a9a9b1e4b4c152098d29e1f63bb049977c445c
Evgeny <e.archibasov@gmail.com>
2022-03-03 07:50:20 +1000

    [Server] Create simple server and client

---
+++ src/server/client.ts
@@ -0,0 +1,37 @@
+import http, { RequestOptions, ClientRequest } from 'http';
+
+const postData = JSON.stringify({
+    msg: 'hello',
+});
+
+const options: RequestOptions = {
+    hostname: 'localhost',
+    port: 3000,
+    method: 'POST',
+    path: '/test',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length,
+    },
+};
+
+const req: ClientRequest = http.request(options, (res) => {
+    console.log('Status: ', res.statusCode);
+    console.log('Headers: ', JSON.stringify(res.headers));
+    res.setEncoding('utf-8');
+
+    res.on('data', (chunk) => {
+        process.stdout.write(`${chunk}\n`);
+    });
+
+    res.on('end', () => {
+        console.log('No more data');
+    });
+});
+
+req.on('error', (e) => {
+    console.log('Error: ', e);
+});
+
+req.write(postData);
+req.end();
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,22 +1,73 @@
-import http, { Server } from 'http';
+import http, { Server, IncomingMessage, ServerResponse } from 'http';
+import { URL } from 'url';
+
+interface Params {
+    req: IncomingMessage;
+    res: ServerResponse;
+    method?: string;
+}
+
+const requestHandler = ({ req, res, method = 'GET' }: Params, response: string) => {
+    if (method === 'POST') {
+        let body = '';
+
+        req.on('data', (data) => {
+            body += data;
+        });
+
+        req.on('end', () => {
+            const post = JSON.parse(body);
+            console.log(post);
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(response);
+        });
+    } else {
+        req.on('end', () => {
+            res.end('Hello from GET request');
+        });
+    }
+};
 
 const port = 3000;
 
 const server: Server = http.createServer();
 
-server.on('request', (req, res) => {
-    console.log('Request event');
+server.on('request', (req: IncomingMessage, res: ServerResponse) => {
+    if (req.url) {
+        const url: URL = new URL(req.url, `http://${req.headers.host}`);
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello');
-});
-
-server.on('connection', () => {
-    console.log('Connection event');
+        if (url.pathname === '/hello') {
+            requestHandler(
+                {
+                    req,
+                    res,
+                    method: req.method,
+                },
+                'Hello from server! /hello'
+            );
+        } else if (url.pathname === '/test') {
+            requestHandler(
+                {
+                    req,
+                    res,
+                    method: req.method,
+                },
+                'Hello from server! /test'
+            );
+        } else {
+            requestHandler(
+                {
+                    req,
+                    res,
+                    method: req.method,
+                },
+                'Hello from server!'
+            );
+        }
+    }
 });
 
 server.listen(port, () => {
-    console.log('Listening event', process.env.USER_ID);
+    console.log('Server start at port:', port);
 });
-
-console.log('Server start at port:', port);
