d2ab3ab22d734b7d1464fb846678a132cc6c164d
Evgeny <e.archibasov@gmail.com>
2022-03-08 15:32:53 +1000

    [dev] Fix precommit hook for server

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
     'frontend/src/**/*.{ts,tsx}': () => 'yarn type-check',
     'frontend/src/**/*.{js,jsx,ts,tsx}': ['yarn prettify', 'yarn lint', 'yarn stylelint'],
-    'server/**/*': () => ['yarn type-check', 'yarn lint'],
+    'server/**/*.js': ['yarn prettify', 'yarn lint'],
 }
